4bdf946ec7f46a79ef13658b26348620ddc919e2
charlycito <profcompu026@gmail.com>
2026-07-20 10:45:21 -0600

    Update sesiones.js

--- manejo-datos/sesiones.js
+++ manejo-datos/sesiones.js
@@ -2,6 +2,6 @@ const sesionesManejoDatos = [
   {
     numero: 1,
     titulo: "Los datos están en todas partes",
-    pdf: "https://correobuap-my.sharepoint.com/:b:/g/personal/jcarlos_arellano_correo_buap_mx/IQCazv1QTc2eSIoqowmWR4fLAZTTYHsuh6mGOcES7pzNH5g?e=Nehszy"
+    pdf: "pdfs/sesion1b1.pdf"
   }
 ];
