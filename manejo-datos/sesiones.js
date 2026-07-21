7c4e2531478160b6ba5e2b5e1d37ee81fcd330e0
charlycito <profcompu026@gmail.com>
2026-07-21 09:21:24 -0600

    Update sesiones.js

--- manejo-datos/sesiones.js
+++ manejo-datos/sesiones.js
@@ -2,8 +2,8 @@ const sesionesManejoDatos = [
   {
     numero: 1,
     titulo: "Los datos están en todas partes",
-    pdf: "https://drive.google.com/file/d/1nW1vdbw10ZiiXtL86bgKkqjqvqjh8Ace/preview",
-    abrir: "https://drive.google.com/file/d/1nW1vdbw10ZiiXtL86bgKkqjqvqjh8Ace/view?usp=sharing",
-    descargar: "https://drive.google.com/uc?export=download&id=1nW1vdbw10ZiiXtL86bgKkqjqvqjh8Ace"
+    pdf: "https://drive.google.com/file/d/1pKgymGHbDeOMdNji5448NfGCAUhdfhEc/preview",
+    abrir: "https://drive.google.com/file/d/1pKgymGHbDeOMdNji5448NfGCAUhdfhEc/view?usp=sharing",
+    descargar: "https://drive.google.com/uc?export=download&id=1pKgymGHbDeOMdNji5448NfGCAUhdfhEc"
   }
 ];
