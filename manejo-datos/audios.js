c845c5495a6c4616da8dfccff7a8102689b0be71
charlycito <profcompu026@gmail.com>
2026-07-21 00:19:06 -0600

    Add audiosManejoDatos array with audio details

---
+++ manejo-datos/audios.js
@@ -0,0 +1,8 @@
+const audiosManejoDatos = [
+  {
+    numero: 1,
+    titulo: "Los datos están en todas partes",
+    spotify: "https://open.spotify.com/embed/episode/6nnOgKDAqaDKQeXkh9H9cg",
+    abrir: "https://open.spotify.com/episode/6nnOgKDAqaDKQeXkh9H9cg"
+  }
+];
