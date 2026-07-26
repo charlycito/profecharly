f649ea40cac90ff8a5a9060f8be918b035cee5d7
charlycito <profcompu026@gmail.com>
2026-07-25 23:47:44 -0600

    Add new session data with resources and materials

--- manejo-datos/sesiones.js
+++ manejo-datos/sesiones.js
@@ -5,5 +5,26 @@ const sesionesManejoDatos = [
     pdf: "https://drive.google.com/file/d/1pKgymGHbDeOMdNji5448NfGCAUhdfhEc/preview",
     abrir: "https://drive.google.com/file/d/1pKgymGHbDeOMdNji5448NfGCAUhdfhEc/view?usp=sharing",
     descargar: "https://drive.google.com/uc?export=download&id=1pKgymGHbDeOMdNji5448NfGCAUhdfhEc"
+  },
+  {
+    numero: 2,
+    titulo: "Diseño del instrumento de recolección de datos",
+    pdf: "https://drive.google.com/file/d/1XiDCvB9CBHZkDclnqDoGUGrTd9Pm_6nu/preview",
+    abrir: "https://drive.google.com/file/d/1XiDCvB9CBHZkDclnqDoGUGrTd9Pm_6nu/view?usp=sharing",
+    descargar: "https://drive.google.com/uc?export=download&id=1XiDCvB9CBHZkDclnqDoGUGrTd9Pm_6nu",
+    materiales: [
+      {
+        tipo: "Instrumento de trabajo",
+        titulo: "Instrumento para la recolección de datos",
+        descripcion: "Documento que utilizarás para organizar y registrar la información de la actividad.",
+        abrir: "https://drive.google.com/file/d/1trhBbW-zblAqW2tK0cZ9Osje8WkXZjg_/view?usp=sharing"
+      },
+      {
+        tipo: "Manual de apoyo",
+        titulo: "Manual del analista: diseño del instrumento de recolección de datos",
+        descripcion: "Guía de consulta para comprender los criterios y pasos del diseño del instrumento.",
+        abrir: "https://drive.google.com/file/d/1hwJeH8PovV4l9bO8BKYBjozeeWXv9llL/view?usp=sharing"
+      }
+    ]
   }
 ];
