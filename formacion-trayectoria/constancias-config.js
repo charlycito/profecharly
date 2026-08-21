b0b02b3f3bdd76985a21a062497d52574687c381
charlycito <profcompu026@gmail.com>
2026-08-21 00:35:51 -0600

    Add files via upload

---
+++ formacion-trayectoria/constancias-config.js
@@ -0,0 +1,3 @@
+// Servicio público de solo lectura que consulta las constancias en Google Drive.
+// Si no responde, la página utiliza automáticamente la copia local de respaldo.
+window.PROFE_CHARLY_CONSTANCIAS_ENDPOINT = "https://script.google.com/macros/s/AKfycbwlQ62NVUFifCkWGowXgEE5dmecGlrq_rQQ2hlp4nyEaWWmzUYOTMh87YwW2YmYWXpTsw/exec";
